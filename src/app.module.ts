1ae2b0b9643f1bbf654d11aa0f6032fa4f7f661e
rmavuluri <rummy.mavuluri@outlook.com>
2023-10-04 22:15:30 -0400

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CoursesModule } from './courses/courses.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IamModule } from './iam/iam.module';
+
+@Module({
+  imports: [
+    CoursesModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    IamModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
